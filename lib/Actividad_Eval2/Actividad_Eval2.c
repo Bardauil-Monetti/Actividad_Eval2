719664f9c804ca888054f896e67787999c27bef9
Bardauil-Monetti <gbardauil548@gmail.com>
2025-08-06 20:34:33 -0300

    Update Actividad_Eval2.c

--- lib/Actividad_Eval2/Actividad_Eval2.c
+++ lib/Actividad_Eval2/Actividad_Eval2.c
@@ -11,18 +11,26 @@ int sec[4][4] = {
     //es una secuencia de paso completo
 };
 int pins[4];
+int sentido = 0; //tambien se puede hacer con variable de tipo bool
 void Actividad_Eval1_init(){
     RCC->APB2ENR|=RCC_APB2ENR_IOPAEN;
-    for(int i = 0; i < 3; i++){
-        //seteamos los pines del PA en 0 para la inicializacion
-        GPIOA -> CRL &= ~(0xF << ((s1 * 4) + i)); 
-        GPIOA -> CRL &= ~(0xF << ((s2 * 4) + i));
-    }
+    GPIOA -> CRL &= ~(0xF << (s1 * 4)); //reset de pines 
+    GPIOA -> CRL &= ~(0xF << (s2 * 4));
     GPIOA -> CRL |= (0b1000 << (s1 * 4)); //los sensores son entradas con pull-up por consigna 
     GPIOA -> CRL |= (0b1000 << (s2 * 4));
+    GPIOA -> BSRR |= (1 << s1);
+    GPIOA -> BSRR |= (1 << s2);
 }
 void mPaP_Init(int IN0, int IN1, int IN2, int IN3){
+    IN0 = 3; // le asigno pines a las bobinas
+    IN1 = 4;
+    IN2 = 5;
+    IN3 = 6;
     int pins[4] = {IN0, IN1, IN2, IN3};
+    for(int i = 0; i < 4; i++){
+        GPIOA -> CRL &= ~(0XF << (pins[i] * 4));//configuro los pines en el PA como entradas flotantes
+        GPIOA -> CRL |= (0b0100 << (pins[i] *4));
+    }
     RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
     for(int i = 0; i < 4; i++){
         if(pins[i] < 8){
@@ -40,36 +48,27 @@ void mPaP_Init(int IN0, int IN1, int IN2, int IN3){
     
 }
 int mPap_Steps(int grados){
-    int sentido = 0; //tambien se puede hacer con variable de tipo bool
-    float pasos = grados/1.8;
     
+    float pasos = grados/1.8;
     for(int i = 0; i < pasos; i++){ //porque debo prender y apagar las bobinas (cantidad de pasos) veces
-        if(grados > 0){ //si el angulo de inclinacion de la barrera es mayor a 0, la barrera debe subir
-            sentido = 1;
-        }else{ //si no, debe bajar
-            sentido = -1;
-            pasos = -pasos; 
-        }
-        if(grados == -60){
-            delay_ms(10000); //se esperan 10 segundos antes de cerrar la barrera
-        }
-        for(int i = 0; i < 4; i++){
-            int orden;
+        for(int j = 0; j < 4; j++){
+           
             if (sentido == 1){
-                for(int j = 0; j < 4; j++){
-                    if(sec[i%4][j]){
-                        GPIOA->BSRR |= (1 << pins[j]);
+                for(int k = 0; k < 4; k++){
+                    if(sec[j%4][k]){
+                        GPIOA->BSRR |= (1 << pins[k]);
                     }else{
-                        GPIOA->BSRR |= (1 << (pins[j] + 16));
+                        GPIOA->BSRR |= (1 << (pins[k] + 16));
                     }
                 }
                 //las bobinas se prenden de la 0 a la 3
-            }else if(sentido == -1){
-                for(int j = 0; j < 4; j++){
-                    if(sec[3-(i%4)][j]){
-                        GPIOA->BSRR |= (1 << pins[j]);
+            }else if(sentido == 0){
+                delay_ms(10000);
+                for(int k = 0; k < 4; k++){
+                    if(sec[3-(j%4)][j]){
+                        GPIOA->BSRR |= (1 << pins[k]);
                     }else{
-                        GPIOA->BSRR |= (1 << (pins[j] + 16));
+                        GPIOA->BSRR |= (1 << (pins[k] + 16));
                     }
                 }
                 //las bobinas del motor se prender en orden inverso (de la 3 a la 0)
@@ -80,13 +79,13 @@ int mPap_Steps(int grados){
 }
 void EXTI0_IRQHandler(void){
     if(EXTI->PR & (1 << s1)){
-        mPap_Steps(60);
+        sentido = 1;
         EXTI->PR |= (1 << s1);
     }
 }
 void EXTI1_IRQHandler(void){
     if(EXTI->PR & (1 << s2)){
-        mPap_Steps(-60);
+        sentido = 0;
         EXTI->PR |= (1 << s2);
     }
 }
